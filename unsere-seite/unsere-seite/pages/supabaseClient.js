5e42c931c0c759ca2b610c2ea193d427a147b349
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-07 03:28:51 +0200

    Create supabaseClient.js

---
+++ unsere-seite/unsere-seite/pages/supabaseClient.js
@@ -0,0 +1,8 @@
+const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co; // Ersetze dies mit deinem Supabase-URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y'; // Dein öffentlicher API-Schlüssel von Supabase
+
+// Supabase-Client initialisieren
+const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
+
+// Exportiere den Supabase-Client
+export default supabase;
