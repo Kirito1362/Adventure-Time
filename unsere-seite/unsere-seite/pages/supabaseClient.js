cee1b5de00695615c2c08561598d2b00cddaa7ba
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-07 03:30:04 +0200

    Update supabaseClient.js

--- unsere-seite/unsere-seite/pages/supabaseClient.js
+++ unsere-seite/unsere-seite/pages/supabaseClient.js
@@ -1,4 +1,4 @@
-const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co; // Ersetze dies mit deinem Supabase-URL
+const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co'; // Ersetze dies mit deinem Supabase-URL
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y'; // Dein öffentlicher API-Schlüssel von Supabase
 
 // Supabase-Client initialisieren
