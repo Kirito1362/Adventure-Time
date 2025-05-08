f0c1c6de753dffaa56580f3c195a34a7937dd46c
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-08 02:11:52 +0200

    Update index.js

--- unsere-seite/unsere-seite/pages/index.js
+++ unsere-seite/unsere-seite/pages/index.js
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import { supabase } from "../supabaseClient";
+import { createClient } from '@supabase/supabase-js';
+
+// Supabase-Client direkt hier erstellen
+const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function Home() {
   const [events, setEvents] = useState([]);
