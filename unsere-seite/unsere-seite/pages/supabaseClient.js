c37be306aa133fa441b2250b128efbbf3a6df1b4
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-08 02:07:05 +0200

    Update supabaseClient.js

--- unsere-seite/unsere-seite/pages/supabaseClient.js
+++ unsere-seite/unsere-seite/pages/supabaseClient.js
@@ -1,21 +1,19 @@
 import React, { useEffect } from 'react';
-
-// Importiere Supabase
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y';
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const SupabaseClient = () => {
   useEffect(() => {
-    // Deine Supabase-Logik hier
     const getData = async () => {
       const { data, error } = await supabase.from('your-table').select('*');
       if (error) {
-        console.error(error);
+        console.error('Fehler beim Abrufen der Daten:', error);
       } else {
-        console.log(data);
+        console.log('Daten:', data);
       }
     };
 
@@ -30,4 +28,4 @@ const SupabaseClient = () => {
   );
 };
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default SupabaseClient;
