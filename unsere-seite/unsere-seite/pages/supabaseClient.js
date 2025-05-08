610e1f1a18dcf6318a451fe4a3351b1da764ef6b
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-08 02:03:48 +0200

    Update supabaseClient.js

--- unsere-seite/unsere-seite/pages/supabaseClient.js
+++ unsere-seite/unsere-seite/pages/supabaseClient.js
@@ -3,8 +3,8 @@ import React, { useEffect } from 'react';
 // Importiere Supabase
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const SupabaseClient = () => {
