92c94a698dcf004f86490eeff776a9f3c66320b8
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-08 02:21:55 +0200

    Update index.js

--- unsere-seite/unsere-seite/pages/index.js
+++ unsere-seite/unsere-seite/pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import { supabase } from "supabaseClient.js";
+import { supabase } from "./supabaseClient.js";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
