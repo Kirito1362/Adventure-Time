311e3c2b26290ab4e30e8ea4808659449479ce21
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-08 02:30:12 +0200

    Update supabaseClient.js

--- unsere-seite/unsere-seite/pages/supabaseClient.js
+++ unsere-seite/unsere-seite/pages/supabaseClient.js
@@ -1,31 +1,11 @@
-import React, { useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
+// Initialisiere den Supabase-Client
 const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const SupabaseClient = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const { data, error } = await supabase.from('events').select('*');
-      if (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-      } else {
-        console.log('Daten:', data);
-      }
-    };
+// Exportiere den Supabase-Client
+export { supabase };
 
-    getData();
-  }, []);
-
-  return (
-    <div>
-      <h1>Supabase Client</h1>
-      <p>Hier kannst du mit Supabase arbeiten!</p>
-    </div>
-  );
-};
-
-export default SupabaseClient;
