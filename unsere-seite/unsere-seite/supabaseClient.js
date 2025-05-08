c78ebe23655c722b9e98460dddb93acced83027a
Kirito1362 <alexehrhardt21@yahoo.de>
2025-05-08 04:16:01 +0200

    Update supabaseClient.js

--- unsere-seite/unsere-seite/supabaseClient.js
+++ unsere-seite/unsere-seite/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Initialisierung des Supabase-Clients
-const supabaseUrl = 'https://hbxfwqacszovbjqulqnh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y';
+const supabaseUrl = "https://hbxfwqacszovbjqulqnh.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhieGZ3cWFjc3pvdmJqcXVscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODAyMzksImV4cCI6MjA2MjE1NjIzOX0.qT9iYElciOS5w-aRJhik5fmCBtSVOUH0p0Drg8R9u7Y";
 
 // Nur einmal instanziieren
 const supabase = createClient(supabaseUrl, supabaseKey);
